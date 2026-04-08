1edb62b9aac726cc2d5141c45c32a91aac88f0af
Dannyk24 <teckydan@gmail.com>
2026-04-08 16:05:29 +0100

    updated product images file path to match new folder strcuture

--- BACKEND/DATA/productsImages.js
+++ BACKEND/DATA/productsImages.js
@@ -3,131 +3,131 @@ export const productsImages = [
     {
         id:1,
         images:[
-            "FRONTEND/ASSETS/IMAGES/headphones-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/headphones-2.jpg",
-            "FRONTEND/ASSETS/IMAGES/headphones-3.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/headphones-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/headphones-2.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/headphones-3.jpg"
         ]
     },
     {
         id:2,
         images:[
-            "FRONTEND/ASSETS/IMAGES/keyboard-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/keyboard-1.jpg"
         ]
     },
     {
         id:3,
         images:[
-            "FRONTEND/ASSETS/IMAGES/gaming-mouse-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/gaming-mouse-1.jpg"
         ]
     },
     {
         id:4,
         images:[
-            "FRONTEND/ASSETS/IMAGES/type-c-cord-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/type-c-cord-1.jpg"
         ]
     },
     {
         id:5,
         images:[
-            "FRONTEND/ASSETS/IMAGES/bluetooth-speaker-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/bluetooth-speaker-1.jpg"
         ]
     },
     {
         id:6,
         images:[
-            "FRONTEND/ASSETS/IMAGES/laptop-stand-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/laptop-stand-1.jpg"
         ]
     },
     {
         id:7,
         images:[
-            "FRONTEND/ASSETS/IMAGES/webcam-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/webcam-1.jpg"
         ]
     },
     {
         id:8,
         images:[
-            "FRONTEND/ASSETS/IMAGES/hard-drive-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/hard-drive-2.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/hard-drive-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/hard-drive-2.jpg"
         ]
     },
     {
         id:9,
         images:[
-            "FRONTEND/ASSETS/IMAGES/powerbank-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/powerbank-1.jpg"
         ]
     },
     {
         id:10,
         images:[
-            "FRONTEND/ASSETS/IMAGES/smartwatch-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/smartwatch-2.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/smartwatch-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/smartwatch-2.jpg"
         ]
     },
     {
         id:11,
         images:[
-            "FRONTEND/ASSETS/IMAGES/monitor-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/monitor-2.jpg",
-            "FRONTEND/ASSETS/IMAGES/monitor-3.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/monitor-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/monitor-2.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/monitor-3.jpg"
         ]
     },
     {
         id:12,
         images:[
-            "FRONTEND/ASSETS/IMAGES/router-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/router-1.jpg"
         ]
     },
     {
         id:13,
         images:[
-            "FRONTEND/ASSETS/IMAGES/earbuds-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/earbuds-1.jpg"
         ]
     },
     {
         id:14,
         images:[
-            "FRONTEND/ASSETS/IMAGES/usb-microphone-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/usb-microphone-2.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/usb-microphone-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/usb-microphone-2.jpg"
         ]
     },
     {
         id:15,
         images:[
-            "FRONTEND/ASSETS/IMAGES/vr-headset-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/vr-headset-2.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/vr-headset-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/vr-headset-2.jpg"
         ]
     },
     {
         id:16,
         images:[
-            "FRONTEND/ASSETS/IMAGES/desk-lamp-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/desk-lamp-1.jpg"
         ]
     },
     {
         id:17,
         images:[
-            "FRONTEND/ASSETS/IMAGES/notebook-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/notebook-2.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/notebook-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/notebook-2.jpg"
         ]
     },
     {
         id:18,
         images:[
-            "FRONTEND/ASSETS/IMAGES/pen-1.jpg",
-            "FRONTEND/ASSETS/IMAGES/pen-2.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/pen-1.jpg",
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/pen-2.jpg"
         ]
     },
     {
         id:19,
         images:[
-            "FRONTEND/ASSETS/IMAGES/desk-organizer-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/desk-organizer-1.jpg"
         ]
     },
     {
         id:20,
         images:[
-            "FRONTEND/ASSETS/IMAGES/daily-planner-1.jpg"
+            "FRONTEND/ASSETS/IMAGES/PRODUCT-IMAGES/daily-planner-1.jpg"
         ]
     }
 ]
