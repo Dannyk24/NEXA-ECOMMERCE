09a62bfcb679060f7bd68fcd9655396715c7f5ec
Dannyk24 <teckydan@gmail.com>
2026-04-13 09:33:01 +0100

    build navbar navigation

---
+++ FRONTEND/SCRIPTS/app.js
@@ -0,0 +1,29 @@
+//HEADER SCROLL INTO VIEW LOGIC
+/*====HELPERS====*/
+function setActiveNavItem(navItem){
+    navItems.forEach(item=>{
+        item.classList.toggle('active-nav-item',
+            item.dataset.section === navItem.dataset.section
+        )/*Toggle takes a force parameter that adds the class if the condition returns true and removes it if the condition returns false*/
+    })
+}
+function scrollSectionIntoView(section){
+    section.scrollIntoView({
+        behavior:'smooth',
+    })
+}
+
+
+const navItemsContainer = document.querySelector('nav')
+const navItems = document.querySelectorAll('.nav-item')
+const sections = document.querySelectorAll('.js-main-sections')
+navItemsContainer.addEventListener('click',(e)=>{
+    if(!e.target.classList.contains('nav-item')){
+        return
+    }
+    const navItem = e.target
+    const sectionsArray = Array.from(sections)//Convert sections nodelist to array to gain access to the find method
+    const section =  sectionsArray.find(section=>section.dataset.section === navItem.dataset.section)
+    setActiveNavItem(navItem)
+    scrollSectionIntoView(section)
+})
