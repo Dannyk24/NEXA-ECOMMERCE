f87edb0f965c47fa263e9eb50781b0293d144452
Dannyk24 <teckydan@gmail.com>
2026-04-15 23:55:03 +0100

    built faq's toggle logic

--- FRONTEND/SCRIPTS/app.js
+++ FRONTEND/SCRIPTS/app.js
@@ -128,5 +128,32 @@ setInterval(() => {
         //Check if its the same as the old one
         pickNewSlideShowImage(); //If it is, pick again
     }
-    setNewSlideShowImage(); //If it isnt set it as the new image
-}, 7000);
+    setNewSlideShowImage(); //If it isnt, set it as the new image
+}, 3000);
+
+//FAQ's SECTION LOGIC
+/*=====HELPERS=====*/
+function expandFaq(faq) {
+    const plusIcon = faq.querySelector(".fa-plus");
+    faq.classList.add("faq-active");
+    plusIcon.classList.remove("fa-plus");
+    plusIcon.classList.add("fa-minus");
+}
+function collapseFaq(faq) {
+    const minusIcon = faq.querySelector(".fa-minus");
+    faq.classList.remove("faq-active");
+    minusIcon.classList.remove("fa-minus");
+    minusIcon.classList.add("fa-plus");
+}
+
+const faqsContainer = document.querySelector(".faqs-container");
+faqsContainer.addEventListener("click", (e) => {
+    //Use event delegation by adding the event listener to the faq's general container
+    const faq = e.target.closest(".faq-container");
+    if (e.target.classList.contains("fa-plus")) {
+        expandFaq(faq);
+    } else if (e.target.classList.contains("fa-minus")) {
+        collapseFaq(faq);
+    }
+    //Check if faq toggle icon is plus, if it is,open it. if its minus,collapse the faq
+});
