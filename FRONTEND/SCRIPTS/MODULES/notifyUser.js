8ccb86863f7671a5090c592bcfcae5743b44c906
Dannyk24 <teckydan@gmail.com>
2026-04-20 23:48:13 +0100

    Implemented dynamic toast data logic

--- FRONTEND/SCRIPTS/MODULES/notifyUser.js
+++ FRONTEND/SCRIPTS/MODULES/notifyUser.js
@@ -1,8 +1,16 @@
+import { capitalize } from "../UTILS/format.js";
+
 let timeoutId;
 export function notfiy(type, message) {
     const notifyContainer = document.querySelector(".toast-container");
     notifyContainer.classList.add(`${type}-toast`); //Add dynamic toast type
     notifyContainer.classList.add("toast-active"); //Make toast visible
+
+    const toastHeader = notifyContainer.querySelector(".toast-header");
+    toastHeader.textContent = capitalize(type);
+
+    const toastMessage = notifyContainer.querySelector(".toast-message");
+    toastMessage.textContent = message;
 
     if (timeoutId) {
         /*Debounce the toast before making it visible to avoid weird behaviour*/
@@ -11,5 +19,5 @@ export function notfiy(type, message) {
     timeoutId = setTimeout(() => {
         notifyContainer.classList.remove("toast-active");
         notifyContainer.classList.remove(`${type}-toast`);
-    }, 3000);
+    }, 4000);
 }
