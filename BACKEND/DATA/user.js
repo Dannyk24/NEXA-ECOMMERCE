870b6646bb0b7f94600d290011ce277226be767f
Dannyk24 <teckydan@gmail.com>
2026-04-21 10:17:32 +0100

    Built active user functionality

--- BACKEND/DATA/user.js
+++ BACKEND/DATA/user.js
@@ -1,16 +1,22 @@
-export const users = getUsers() || [
+export let activeUser = getActiveUser() || {
+    id: null,
+    username: null,
+    role: null
+}; //Stores the current active user for authorization and giving each user individual carts and other features
+
+export let users = getUsers() || [
     {
         id: 1,
         username: "danieluser",
         email: "teckydan@gmail.com",
-        password: "12345",
+        password: "123456",
         role: "user"
     },
     {
         id: 2,
         username: "danieladmin",
         email: "olajiredaniel9002@gmail.com",
-        password: "12345",
+        password: "123456",
         role: "admin"
     }
 ];
@@ -20,4 +26,15 @@ export function saveUsers() {
 }
 function getUsers() {
     return JSON.parse(localStorage.getItem("users"));
+}
+export function setActiveUser(id, username, role) {
+    activeUser.id = id;
+    activeUser.username = username;
+    activeUser.role = role;
+}
+export function saveActiveUser() {
+    sessionStorage.setItem("active-user", JSON.stringify(activeUser));
+}
+function getActiveUser() {
+    return JSON.parse(sessionStorage.getItem("active-user")); //If theres no activeUser,return null
 }
