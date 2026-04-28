64f97eda553264c45105e5417cf707a8f2687af9
Dannyk24 <teckydan@gmail.com>
2026-04-28 13:33:28 +0100

    built functional user homepage mobile sidebar toggle

--- FRONTEND/SCRIPTS/USER/homepage.js
+++ FRONTEND/SCRIPTS/USER/homepage.js
@@ -4,17 +4,32 @@ import {
     blockUserScrolling,
     restoreUserScrolling
 } from "../MODULES/overlay.js";
+import { activeUser, checkActiveUser } from "../../../BACKEND/DATA/user.js";
+
+if (!checkActiveUser()) {
+    document.body.innerHTML = "YOU MUST BE AUTHENTICATED TO VIEW THIS PAGE";
+}
+
+/*Mobile sidebar toggle*/
+
+/*=====HELPERS=====*/
+function openMobileNav() {
+    authenticatedNavbar.classList.add("authenticated-mobile-nav-active");
+    openOverlay();
+    blockUserScrolling();
+}
+function closeMobileNav() {
+    authenticatedNavbar.classList.remove("authenticated-mobile-nav-active");
+    closeOverlay();
+    restoreUserScrolling();
+}
 
 const sidebarToggle = document.querySelector(".menu-toggle-mobile");
 const authenticatedNavbar = document.querySelector(".authenticated-mobile-nav");
 const overlay = document.querySelector(".overlay");
 sidebarToggle.addEventListener("click", () => {
-    authenticatedNavbar.classList.add("authenticated-mobile-nav-active");
-    openOverlay();
-    blockUserScrolling();
+    openMobileNav();
 });
 overlay.addEventListener("click", () => {
-    authenticatedNavbar.classList.remove("authenticated-mobile-nav-active");
-    closeOverlay();
-    restoreUserScrolling();
+    closeMobileNav();
 });
