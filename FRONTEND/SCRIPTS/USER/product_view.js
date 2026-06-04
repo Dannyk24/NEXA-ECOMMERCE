71e397c339e3738e04679f2cd41c2bbae906f48a
Dannyk24 <teckydan@gmail.com>
2026-06-04 07:06:32 +0100

    modularized back button component and moved order status styles to style.css

--- FRONTEND/SCRIPTS/USER/product_view.js
+++ FRONTEND/SCRIPTS/USER/product_view.js
@@ -103,14 +103,6 @@ const productImagesContainer = document.querySelector(
     ".products-images-container"
 ); //Get all html elements after populating page with layout template first
 
-const backBtn = document.querySelector(".back-button");
-backBtn.addEventListener("click", (e) => {
-    if (history.length > 0) {
-        e.preventDefault();
-        history.back();
-    }
-}); //Check if theres a previous page in te browser history, if there is, go back to it, if there isnt, the default action is triggered which takes the user back to the homepage
-
 /*====HELPERS=====*/
 function setActiveImage(image) {
     const activeImage = document.querySelector(".active-image");
