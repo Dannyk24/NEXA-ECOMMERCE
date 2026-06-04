07ac42df12317293d39fb158f030aebe1b28dc10
Dannyk24 <teckydan@gmail.com>
2026-06-04 07:05:34 +0100

    built functional orders page

---
+++ FRONTEND/SCRIPTS/USER/orders.js
@@ -0,0 +1,154 @@
+import {
+    activeUserOrders,
+    cancelOrder,
+    getOrder,
+    setActiveViewingOrder,
+    viewOrder
+} from "../../../BACKEND/DATA/orders.js";
+import dayjs from "https://cdn.jsdelivr.net/npm/dayjs@1.11.13/+esm";
+import { getProduct } from "../../../BACKEND/DATA/productsMethods.js";
+import { getProductImage } from "../../../BACKEND/DATA/productsMethods.js";
+import { openModal, closeModal } from "../MODULES/modals.js";
+import { notify } from "../MODULES/notifyUser.js";
+
+const ordersContainer = document.querySelector(".orders-list");
+function renderOrders() {
+    ordersContainer.innerHTML = "";
+    if (activeUserOrders.length === 0) {
+        ordersContainer.textContent = "You have no orders";
+        return;
+    }
+    const sortedOrders = activeUserOrders.slice().reverse();
+    sortedOrders.forEach((order) => {
+        const orderDate = dayjs(order.orderDate).format("MMM D, YYYY");
+        const deliveryDate = dayjs(order.deliveryDate).format("MMM D, YYYY");
+        ordersContainer.innerHTML += `
+            <div class="order-container" data-id = "${order.id}">
+                <p>Order ID: <span class="order-id">${order.id}</span></p>
+                <div class="order-products-list-container">
+                    <p>Products list</p>
+                    <div class="order-products-list">
+                        
+                    </div>
+                </div>
+                <div class="order-placement-date-container">order date: <span class="order-date">${orderDate}</span>
+                </div>
+                <div class="order-delivery-date-container">delivery date: <span class="delivery-date">${deliveryDate}</span></div>
+                <p class="order-cancellation-notice">You can cancel a pending order before it gets confirmed</p>
+                <div class="order-cta-container">
+                    <div class="order-status ${order.status}-order-status">${order.status}</div>
+                    <div class="order-cancel-button" data-modal="confirmation-modal">
+                        cancel order
+                    </div>
+                </div>
+            </div>
+        `;
+        renderOrderProductsImage(order);
+        const orderContainer = document.querySelector(
+            `[data-id = "${order.id}"]`
+        );
+        handleContainerState(order, orderContainer);
+    });
+}
+
+function handleContainerState(order, orderContainer) {
+    const cancellationNotice = orderContainer.querySelector(
+        ".order-cancellation-notice"
+    );
+    const orderCancelButton = orderContainer.querySelector(
+        ".order-cancel-button"
+    );
+    if (order.status !== "pending") {
+        cancellationNotice.style.display = "none";
+        orderCancelButton.style.display = "none";
+    }
+}
+
+function renderOrder(id) {
+    const orderContainer = document.querySelector(`[data-id = "${id}"]`);
+    const order = getOrder(id);
+    const orderDate = dayjs(order.orderDate).format("MMM D, YYYY");
+    const deliveryDate = dayjs(order.deliveryDate).format("MMM D, YYYY");
+    orderContainer.innerHTML = `
+        <p>Order ID: <span class="order-id">${order.id}</span></p>
+        <div class="order-products-list-container">
+            <p>Products list</p>
+            <div class="order-products-list">
+                
+            </div>
+        </div>
+        <div class="order-placement-date-container">order date: <span class="order-date">${orderDate}</span>
+        </div>
+        <div class="order-delivery-date-container">delivery date: <span class="delivery-date">${deliveryDate}</span></div>
+        <p class="order-cancellation-notice">You can cancel a pending order before it gets confirmed</p>
+        <div class="order-cta-container">
+            <div class="order-status ${order.status}-order-status">${order.status}</div>
+            <div class="order-cancel-button" data-modal="confirmation-modal">
+                cancel order
+            </div>
+        </div>
+    `;
+    renderOrderProductsImage(order);
+    handleContainerState(order, orderContainer);
+}
+
+function renderOrderProductsImage(order) {
+    const orderContainer = document.querySelector(`[data-id = "${order.id}"]`);
+    const orderContainerProductsList = orderContainer.querySelector(
+        ".order-products-list"
+    );
+    order.products.forEach((orderItem) => {
+        const product = getProduct(orderItem.id);
+        const productImage = getProductImage(product);
+        orderContainerProductsList.innerHTML += `<img src="../../../${productImage}" alt="">`;
+    });
+}
+
+renderOrders();
+
+const confirmationModal = document.querySelector("#confirmation-modal");
+ordersContainer.addEventListener("click", (e) => {
+    if (e.target.closest(".order-cancel-button")) {
+        const cancelButton = e.target.closest(".order-cancel-button");
+        const orderContainer = cancelButton.closest(".order-container");
+        const orderId = orderContainer.dataset.id;
+        openModal(cancelButton.dataset.modal);
+        confirmationModal.dataset.id = orderId;
+    } else if (e.target.closest(".order-container")) {
+        const orderContainer = e.target.closest(".order-container");
+        const order = getOrder(Number(orderContainer.dataset.id));
+        viewOrder(order);
+    }
+});
+
+const overlay = document.querySelector(".overlay");
+overlay.addEventListener("click", () => {
+    closeModal("confirmation-modal");
+});
+const confrimationModalCloseButton = confirmationModal.querySelector(
+    ".modal-close-button"
+);
+confrimationModalCloseButton.addEventListener("click", () => {
+    closeModal("confirmation-modal");
+});
+
+confirmationModal.addEventListener("click", (e) => {
+    if (e.target.closest) {
+    }
+});
+
+const cancelOrderConfirmationButton = document.querySelector(
+    ".cancel-order-confirmation-button"
+);
+
+cancelOrderConfirmationButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const orderId = Number(confirmationModal.dataset.id);
+    const orderContainer = document.querySelector(`[data-id = "${orderId}"]`);
+    if (!cancelOrder(orderId)) {
+        return;
+    }
+    renderOrder(orderId);
+    closeModal("confirmation-modal");
+    notify("success", "Order cancelled");
+});
