bce4c7ad4b0880ce6c66b088bd92b9ba44166f8e
Dannyk24 <teckydan@gmail.com>
2026-06-06 09:26:12 +0100

    added search faq and authentication block for unauthenticated users

--- FRONTEND/SCRIPTS/MODULES/main.js
+++ FRONTEND/SCRIPTS/MODULES/main.js
@@ -1,3 +1,7 @@
 import { updateOrders } from "../../../BACKEND/DATA/orders.js";
+import { activeUser } from "../../../BACKEND/DATA/user.js";
 
+if (!activeUser.id) {
+    document.body.innerHTML = "YOU MUST BE AUTHENTICATED TO VIEW THIS PAGE";
+}
 updateOrders();
